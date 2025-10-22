42db11498700f253e623c36d233b53d080feb151
Esther nekesa <esthernekesasi32@gmail.com>
2025-10-22 09:36:02 +0300

    Complete React Router setup and pass all tests

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,10 @@
-
-
-function UserCard({user}) {
+function UserCard({ user }) {
   return (
     <article>
-        <h2>{user.name}</h2>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
     </article>
   );
-};
+}
 
 export default UserCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,30 +2,23 @@ import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
 
 function Home() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Error fetching users:", err));
   }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>
-  });
+
+  const userList = users.map((user) => <UserCard key={user.id} user={user} />);
 
   return (
-    <>
-      <header>
-        {/* place NavBar here */}
-      </header>
-      <main>
-        <h1>Home!</h1>
-        {userList}
-      </main>
-    </>
+    <main>
+      <h1>Home!</h1>
+      {userList}
+    </main>
   );
-};
+}
 
 export default Home;
